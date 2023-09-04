8e749a8690fc7ee95c52cfc8afebdd8953983a4e
DanielRicra <daniel.int@outlook.com>
2023-09-04 15:56:17 -0500

    Update ChatHeader to put user avatar
    - Show user avatar on prop type "conversation".
    
    - Modify the prop type to make it conditional for types.

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,13 +1,21 @@
 import { channel } from "diagnostics_channel";
 import { HashIcon, MenuIcon } from "lucide-react";
 import MobileToggle from "../mobile-toggle";
+import UserAvatar from "../user-avatar";
 
-interface ChatHeaderProps {
+interface ChannelProps {
 	serverId: string;
 	name: string;
-	type: "channel" | "conversation";
+	type: "channel";
 	imageUrl?: string;
 }
+
+interface ConversationProps extends Omit<ChannelProps, "type"> {
+	type: "conversation";
+	imageUrl: string;
+}
+
+type ChatHeaderProps = ChannelProps | ConversationProps;
 
 const ChatHeader: React.FC<ChatHeaderProps> = ({
 	name,
@@ -17,10 +25,12 @@ const ChatHeader: React.FC<ChatHeaderProps> = ({
 }) => {
 	return (
 		<div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <MobileToggle serverId={serverId} />
+			<MobileToggle serverId={serverId} />
 			{type === "channel" && (
 				<HashIcon className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
 			)}
+
+			{type === "conversation" && <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />}
 
 			<p className="font-semibold text-md text-black dark:text-white">{name}</p>
 		</div>
